0d0c02d86c73c658330f6cdc87bf301540f2a4fd
Marek Urzon <marekurzon@gmail.com>
2017-10-11 22:38:46 +0200

    Added portfolio items via Resource generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
-  get 'pages/about'
-
-  get 'pages/contact'
-  
+  resources :portfolios
   resources :blogs
+
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/contact'
 end
